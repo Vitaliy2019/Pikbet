f3cb131513670ab80e117275863d88f2ac292f4d
acrtr <pristupa@acrtr.ru>
2019-08-30 23:14:54 +0300

    api.pikbet.ru

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,8 +22,8 @@ module.exports = {
    },*/
   axios: {
     // baseURL: // process.env.NODE_ENV !== 'production' ? 'http://localhost:59501' : ''
-    baseURL: 'https://localhost:5001'
-    // baseURL: 'https://api.acrtr.ru'
+    //baseURL: 'https://localhost:5001'
+    baseURL: 'https://api.pikbet.ru'
     // baseURL: apiUrl
   },
   auth: {
